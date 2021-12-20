a060215ed49705c568859331c7d195d9334a436d
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2021-12-21 01:49:52 +0300

    added condition for equal numbers

--- NumberOrders.java
+++ NumberOrders.java
@@ -20,23 +20,27 @@ public class NumberOrders {
         System.out.print("c sayısı: ");
         c = input.nextInt();
 
-        if (a > b && a > c) {
-            if (b > c) {
-                System.out.println("a > b > c");
-            } else {
-                System.out.println("a > c > b");
-            }
-        } else if ( b > a && b > c) {
-            if (a > c) {
-                System.out.println("b > a > c");
-            } else {
-                System.out.println("b > c > a");
-            }
+        if (a == b || a == c || b == c) {
+            System.out.println("Lütfen farklı sayılar giriniz.");
         } else {
-            if (a > b) {
-                System.out.println("c > a > b");
+            if (a > b && a > c) {
+                if (b > c) {
+                    System.out.println("a > b > c");
+                } else {
+                    System.out.println("a > c > b");
+                }
+            } else if ( b > a && b > c) {
+                if (a > c) {
+                    System.out.println("b > a > c");
+                } else {
+                    System.out.println("b > c > a");
+                }
             } else {
-                System.out.println("c > b > a");
+                if (a > b) {
+                    System.out.println("c > a > b");
+                } else {
+                    System.out.println("c > b > a");
+                }
             }
         }
     }
