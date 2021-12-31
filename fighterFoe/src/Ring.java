32c1c7afb15fed3db28172456d908009a0306ce5
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-01 01:15:14 +0300

    add isWinner() method

--- fighterFoe/src/Ring.java
+++ fighterFoe/src/Ring.java
@@ -12,4 +12,20 @@ public class Ring {
     {
         return (f1.weight/f2.weight <= 1.1 && f1.weight/f2.weight >= 0.9);
     }
+
+    public boolean isWinner()
+    {
+        if (f1.health == 0)
+        {
+            System.out.println(f2.name + " is the winner!");
+            return true;
+        } else if (f2.health == 0)
+
+        {
+            System.out.println(f1.name + " is the winner!");
+            return true;
+        }
+
+        return false;
+    }
 }
