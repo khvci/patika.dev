68f6109f24ec598d5615adf0422fed7466fd5bb6
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-01 00:47:25 +0300

    add Fighter attributes and constructor

--- fighterFoe/src/Fighter.java
+++ fighterFoe/src/Fighter.java
@@ -1,2 +1,17 @@
 public class Fighter {
+    String name;
+    byte damage;
+    byte defence;
+    byte health;
+    byte weight;
+
+    public Fighter(String name, byte damage, byte defence, byte health, byte weight)
+    {
+        this.name = name;
+        this.damage = damage;
+        this.defence = defence;
+        this.health = health;
+        this.weight = weight;
+    }
+
 }
