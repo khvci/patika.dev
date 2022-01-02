cd356fe35bac0c1125ad9da4f6520a50a4d45d34
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-03 00:22:41 +0300

    move project to a new directory

---
+++ WageCalculator/src/Employee.java
@@ -0,0 +1,52 @@
+public class Employee {
+    String employeeName;
+    double salary;
+    int workHours;
+    int contractYear;
+    int currentYear;
+
+    public Employee(String employeeName, double salary, int workHours, int contractYear) {
+        this.employeeName = employeeName;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.contractYear = contractYear;
+        this.currentYear = 2022;
+    }
+
+    public float bonus() {
+        if (this.workHours > 40) {
+            salary += (this.workHours - 40) * 30;
+        }
+    }
+
+    public float raiseSalary() {
+        if (this.currentYear - this.contractYear < 10) {
+            this.salary *= 1.05;
+        } else if (this.currentYear - this.contractYear < 20) {
+            this.salary *= 1.1;
+        } else {
+            this.salary *= 1.15;
+        }
+
+        bonus();
+        tax();
+        toString();
+    }
+
+    public float tax() {
+        if (this.salary > 1000) {
+            this.salary *= 0.97;
+        }
+    }
+
+    public String toString() {
+        System.out.println("Name:\t");
+        System.out.println("Current Salary:\t");
+        System.out.println("Work Hours:.\t");
+        System.out.println("Contract Year\t");
+        System.out.println("New Salary:\t");
+        return null;
+    }
+
+
+}
