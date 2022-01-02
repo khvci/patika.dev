5767f5fbf4550b344f8c09c503b5f4c14eb992aa
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-02 23:46:36 +0300

    add constructor method

--- WageCalculator.java
+++ WageCalculator.java
@@ -4,5 +4,13 @@ public class WageCalculator {
         float salary;
         byte workHours;
         short contractYear;
+
+        public Employee(String employeeName, float salary, byte workHours, short contractYear) {
+            this.employeeName = employeeName;
+            this.salary = salary;
+            this.workHours = workHours;
+            this.contractYear = contractYear;
+        }
     }
+
 }
