3bed38c6943c84d3fbdd034789b855f8b7dbdcff
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-12 00:44:11 +0300

    add functioon to check closest numbers

--- ClosestNumberCheck.java
+++ ClosestNumberCheck.java
@@ -1,4 +1,4 @@
-// Write a program that finds closest two numbers (lower and higher) to user input.
+// Write a program that finds closest two numbers (lower and higher) than user input.
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -10,21 +10,38 @@ public class ClosestNumberCheck {
         System.out.print("Please provide a number to check: ");
         int userInput = input.nextInt();
 
-        int[] array = {56,34,1,8,101,-2,-33};
-        System.out.println(array.length);
+        int[] array = {15,12,788,1,-1,-778,2,0};
 
+        closestNumbers(array, userInput);
+    }
+
+    static void closestNumbers(int[] array, int userInput) {
         int[] sortedArray = Arrays.copyOf(array, array.length);
         Arrays.sort(sortedArray);
 
+        System.out.println("Array is " + Arrays.toString(array));
 
-/*
+        if (userInput <= sortedArray[0] || userInput >= sortedArray[sortedArray.length - 1]) {
+            System.out.println("You provided a number out of range. Please provide a number between "
+            + sortedArray[0] + " and " + sortedArray[sortedArray.length - 1] + " (both excluded)");
+        } else {
+            int lowerClosest = sortedArray[0];
+            int higherClosest = sortedArray[sortedArray.length - 1];
 
-        int lowerClosest, higherClosest;
+            for (int i : sortedArray) {
+                if (i < userInput) {
+                    lowerClosest = i;
+                }
+            }
 
-        for (int i : array) {
+            for (int j = sortedArray.length - 1; j >= 0; j--) {
+                if (sortedArray[j] > userInput) {
+                    higherClosest = sortedArray[j];
+                }
+            }
 
+            System.out.println("Lower closest number is " + lowerClosest);
+            System.out.println("Higher closest number is " + higherClosest);
         }
-*/
-
     }
 }
