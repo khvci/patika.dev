87dba9d92f983de4842a075b76760f1d59067f6a
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-13 19:34:28 +0300

    improve aesthetics

--- LetterPrinter.java
+++ LetterPrinter.java
@@ -5,7 +5,11 @@ public class LetterPrinter {
         for (int i = 0; i < letter.length; i++) {
             for (int j = 0; j < letter[i].length; j++) {
                 if (i == 0 || i == 3 || i == 6) {
-                    letter[i][j] = " * ";
+                    if (j != 3) {
+                        letter[i][j] = " * ";
+                    } else {
+                        letter[i][j] = "   ";
+                    }
                 } else if (j == 0 || j == 3) {
                     letter[i][j] = " * ";
                 } else {
