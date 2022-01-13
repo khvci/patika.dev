da681fb592ec189f943e918eed1cb185a443817c
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-13 19:38:42 +0300

    add task description

--- LetterPrinter.java
+++ LetterPrinter.java
@@ -1,3 +1,5 @@
+// write a program that prints a huge letter B on console
+
 public class LetterPrinter {
     public static void main(String[] args) {
         String[][] letter = new String[7][4];
