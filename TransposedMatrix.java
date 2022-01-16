7a691daf845570db55f07c64415ce84405668cbe
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-16 23:52:14 +0300

    add sample arrays to transpose

--- TransposedMatrix.java
+++ TransposedMatrix.java
@@ -1,10 +1,16 @@
+import java.util.Arrays;
+
 public class TransposedMatrix {
     public static void main(String[] args) {
+        int[][] matrix1 = {{2,3,4},{5,6,4}};
+        int[][] matrix2 = {{1,2,3},{4,5,6}};
 
+        System.out.println(Arrays.deepToString(transpose(matrix1)));
+        System.out.println(Arrays.deepToString(transpose(matrix2)));
     }
 
     static int[][] transpose(int[][] arr) {
-        int[][] transposedMatrix = new int[arr.length][arr[0].length];
+        int[][] transposedMatrix = new int[arr[0].length][arr.length];
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
