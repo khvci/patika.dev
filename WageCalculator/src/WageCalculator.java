aa00f2a902bfd07912d9fe33dc89cc5f5b48d410
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-01-24 14:05:29 +0300

    rename WageCalculator class

--- WageCalculator/src/WageCalculator.java
+++ WageCalculator/src/WageCalculator.java
@@ -1,4 +1,4 @@
-public class Main {
+public class WageCalculator {
     public static void main(String[] args) {
         Employee emp1 = new Employee("John", 2000, 45, 1985);
         Employee emp2 = new Employee("Chris", 2000, 45, 2019);
